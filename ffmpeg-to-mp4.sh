39f4d72d11d291e3dcaf4c84f67d490da833a9a1
Nathan DeMasie <nathan.demasie@gmail.com>
2021-11-06 00:04:26 +0100

    feat(scripts): add ffmpeg-to-mp4

---
+++ ffmpeg-to-mp4.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+command -v "/usr/bin/ffmpeg" >/dev/null 2>&1 || { echo "ffmpeg is not installed"; exit 1; }
+
+$input="$1"
+
+path="$(dirname "$input")"
+filename="$(basename "$input")"
+file="$(filename%.*)"
+
+
+/usr/bin/ffmpeg -i "$input" "$path/$file.mp4"
