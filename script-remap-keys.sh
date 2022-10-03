dff5ddd4cc780d7c04c3bc8513c8fa25f4cc9901
ndemasie <nathan.demasie@gmail.com>
2022-10-03 14:12:32 +0200

    chore: add comment

--- script-remap-keys.sh
+++ script-remap-keys.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# stackoverflow post
 # https://apple.stackexchange.com/questions/283252/how-do-i-remap-a-key-in-macos-sierra-e-g-right-alt-to-right-control
+
+# Key Table
+# https://developer.apple.com/library/archive/technotes/tn2450/_index.html#//apple_ref/doc/uid/DTS40017618-CH1-KEY_TABLE_USAGES
 
 # Keypad Num Lock (0x53) -> PageUp (0x4B)
 # Print Screen (0x46) -> PageDown (0x4E)
