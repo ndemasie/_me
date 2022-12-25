4b8cfd7e9adbdd0d75e5c6fc399f6861d1f9e9ef
ndemasie <nathan.demasie@gmail.com>
2022-12-25 16:39:07 +0100

    feature: refactor and update script

--- script-gitmoji-commit.sh
+++ script-gitmoji-commit.sh
@@ -7,48 +7,31 @@ set -o pipefail  # don't hide errors within pipes
 # COLORS
 readonly C_RESET='\033[0m'
 readonly C_BOLD="\033[1m"
-readonly C_FAINT="\033[2m"
-readonly C_ITALIC="\033[3m"
-readonly C_UNDERLINED="\033[4m"
 
 readonly C_BLACK="\033[30m"
-readonly C_BLUE="\033[34m"
 readonly C_CYAN="\033[36m"
-readonly C_GRAY="\033[90m"
 readonly C_GREEN="\033[32m"
 readonly C_MAGENTA="\033[35m"
 readonly C_RED="\033[31m"
 readonly C_WHITE="\033[37m"
 readonly C_YELLOW="\033[33m"
 
-readonly C_BLUE_LIGHT="\033[94m"
 readonly C_CYAN_LIGHT="\033[96m"
 readonly C_GRAY_LIGHT="\033[97m"
-readonly C_GREEN_LIGHT="\033[92m"
-readonly C_MAGENTA_LIGHT="\033[95m"
 readonly C_RED_LIGHT="\033[91m"
 readonly C_YELLOW_LIGHT="\033[93m"
 
 readonly C_BLACK_BG="\033[40m"
-readonly C_BLUE_BG="\033[44m"
-readonly C_CYAN_BG="\033[46m"
-readonly C_GRAY_BG="\033[100m"
 readonly C_GREEN_BG="\033[42m"
-readonly C_BLUE_LIGHT_BG="\033[104m"
 readonly C_CYAN_LIGHT_BG="\033[106m"
-readonly C_GREEN_LIGHT_BG="\033[102m"
-readonly C_GRAY_LIGHT_BG="\033[47m"
-readonly C_MAGENTA_LIGHT_BG="\033[105m"
-readonly C_RED_LIGHT_BG="\033[101m"
 readonly C_YELLOW_LIGHT_BG="\033[103m"
 readonly C_MAGENTA_BG="\033[45m"
 readonly C_RED_BG="\033[41m"
-readonly C_WHITE_BG="\033[107m"
-readonly C_YELLOW_BG="\033[43m"
 
 readonly CLEAR_LINE="\033[K"
 
 # CONSTS
+readonly TICKET_NUMBER_LEN=3
 readonly GIT_MAIN_BRANCH="master"
 # readonly GIT_EXCLUDE_REGEX=".env\.*"
 readonly GITMOJI_OPTIONS=(
@@ -99,8 +82,8 @@ readonly GITMOJI_OPTIONS=(
 )
 
 # FLAGS
-DEBUG=false
-VERBOSE=false
+declare DEBUG=false
+declare VERBOSE=false
 
 # Generic Helpers
 usage() {
@@ -140,176 +123,6 @@ log() {
   esac
 
   return 0
-}
-
-menu() {
-  echo >&2
-
-  get_cursor_position() { declare POS; read -sdR -p $'\033[6n' POS; echo $POS | cut -c3-; }
-  min() { printf "%s\n" "${@:2}" | sort "$1" | head -n1; }
-  max() { printf "%s\n" "${@:2}" | sort "$1" | tail -n1; }
-  divide_round_up() { echo $((($1 + $2 - 1) / $2)); }
-  divide_round_down() { echo "$(awk "BEGIN {print int($1 / $2)}")"; }
-  # TODO: Seems to be and escaping issue where the key must assigned to a value
-  # DO: `declare key="$(read_keyboard)"; case "$key" in` ...)
-  # DON'T: `case "$(read_keyboard)" in` ...)
-  read_keyboard() {
-    declare k1
-    declare k2
-    declare k3
-    declare k4
-    declare k5
-
-    IFS=''
-    read -r -s -n1 k1
-    [[ $k1 == $'\033' ]] && read -r -s -n1 k2
-    [[ $k2 == [ ]] && read -r -s -n1 k3
-    [[ $k3 == [0-9] ]] && read -r -s -n1 k4
-    [[ $k4 == [0-9] ]] && read -r -s -n1 k5
-    unset IFS
-
-    declare key="${k1:-}${k2:-}${k3:-}${k4:-}${k5:-}"
-    case "$key" in
-      $'\033[A') echo UP ;;
-      $'\033[B') echo DOWN ;;
-      $'\033[D') echo LEFT ;;
-      $'\033[C') echo RIGHT ;;
-      $'\177') echo BACKSPACE ;;
-      $'\0') echo ENTER;;
-      *) echo "$key" ;;
-    esac
-  }
-
-  readonly OPTIONS=("$@")
-  readonly PAGE_SIZE=10
-  readonly LIST_LEN=$(min -g $PAGE_SIZE ${#OPTIONS[@]})
-  readonly HEADER_LEN=2
-  readonly FOOTER_LEN=2
-  readonly FILL_EMPTY=$({
-    declare max
-    for opt in "${OPTIONS[@]}"; do
-      [[ "${#opt}" -gt "$max" ]] && max="${#opt}"
-    done
-    printf '%*s' $max
-  })
-  readonly CURSOR_POS=$(get_cursor_position)
-
-  declare options=()
-  for opt in "${OPTIONS[@]}"; do
-    options+=("$(echo "$opt" | sed -e 's/  / /g' | tr '[[:upper:]]' '[[:lower:]]')")
-  done
-  declare options_filtered=()
-  declare page=0
-  declare search=""
-  declare selected=0
-
-  get_paged_index() { declare i=$(($page * $PAGE_SIZE + $selected)); [[ "${i}" -lt 0 ]] && echo "0" || echo "$i"; }
-  get_selected_option() { echo "${options_filtered[$(get_paged_index)]:-}"; }
-  go_to_home() { printf "\033[$((${CURSOR_POS%;*}+${1:-0}));${2:-0}H" >&2; }
-  go_to_search() { go_to_home 0 "$((9+${#search}))"; }
-  print_option() { printf "${CLEAR_LINE}${C_RESET} %s\n" "$1" >&2; }
-  print_option_selected() { printf "${C_MAGENTA_BG}  %s%s  ${C_RESET}\n" "$1" "${FILL_EMPTY:${#1}}" >&2; }
-
-  draw() {
-    options_filtered=()
-    declare lower_search=$(echo $search | tr '[[:upper:]]' '[[:lower:]]')
-
-    for index in "${!OPTIONS[@]}"; do
-      if [[ "${options[$index]}" == *${lower_search}* ]]; then
-        options_filtered+=("${OPTIONS[$index]}")
-      fi
-    done
-
-    declare options_len="${#options_filtered[@]}"
-    declare visible_count=$(min -g "$options_len" $(($options_len - ($PAGE_SIZE * $page))))
-
-    if [[ $selected -ge $visible_count ]]; then
-      selected=$(($visible_count-1))
-    fi
-
-    go_to_home 0
-    printf "${CLEAR_LINE}${C_RESET}%s: %s\n" "Search" "$search" >&2
-    printf "${CLEAR_LINE}${C_RESET}\n" >&2
-
-    for (( i=0; i<$LIST_LEN; i++ )) {
-      declare page_i=$(($page * $PAGE_SIZE + $i))
-      declare option="${options_filtered[$page_i]:-}"
-      if [[ $page_i == $(get_paged_index) ]]; then
-        print_option_selected "$option"
-      else
-        print_option "$option"
-      fi
-    }
-
-    printf "${CLEAR_LINE}${C_RESET}\n" >&2
-    printf "${CLEAR_LINE}${C_RESET}%s %d-%d / %d\n" \
-      "Results:" \
-      $(( $PAGE_SIZE * $page + 1 )) \
-      $(min -g $(($PAGE_SIZE * ($page+1))) "$options_len") \
-      "$options_len" >&2
-
-    go_to_search
-  }
-
-  draw
-  while true; do
-    # NOTE: `read_keyboard` out must be assigned. Inline use has escaping issue.
-    declare key=$(read_keyboard)
-    case "$key" in
-      ENTER)
-        declare opt="$(get_selected_option)"
-        [[ "$opt" != "" ]] && {
-          go_to_home "$(($HEADER_LEN + $LIST_LEN + $FOOTER_LEN))" 0
-          echo >&2
-          echo "$opt"
-          return
-        }
-        ;;
-      UP)
-        go_to_home "$(($HEADER_LEN + $selected))"
-        print_option "$(get_selected_option)"
-
-        ((selected--))
-        [[ "$selected" -lt 0 ]] && selected=$(($(min -g $(max -g "${#options_filtered[@]}" 1) $LIST_LEN) - 1))
-
-        go_to_home "$(($HEADER_LEN + $selected))"
-        print_option_selected "$(get_selected_option)"
-        go_to_search
-        ;;
-      DOWN)
-        go_to_home "$(($HEADER_LEN + $selected))"
-        print_option "$(get_selected_option)"
-
-        ((selected++))
-        [[ "$selected" -ge $(min -g "${#options_filtered[@]}" $LIST_LEN) ]] && selected=0
-
-        go_to_home "$(($HEADER_LEN + $selected))"
-        print_option_selected "$(get_selected_option)"
-        go_to_search
-        ;;
-      LEFT)
-        ((page--))
-        [[ "$page" -lt 0 ]] && page=$(divide_round_down ${#options_filtered[@]} $PAGE_SIZE)
-        draw
-        ;;
-      RIGHT)
-        ((page++))
-        [[ "$page" -gt "$(divide_round_down ${#options_filtered[@]} $PAGE_SIZE)" ]] && page=0
-        draw
-        ;;
-      BACKSPACE)
-        [[ "${#search}" -gt 0 ]] && {
-          search="${search:0:((${#search}-1))}"
-          draw
-        }
-        ;;
-      *)
-        page=0
-        search="${search}${key}"
-        draw
-        ;;
-    esac
-  done
 }
 
 # Script layout
@@ -370,6 +183,190 @@ ask_yn() {
   echo "${REPLY}" | tr '[:upper:]' '[:lower:]' | cat
 }
 
+menu() {
+  declare FLAG_SEARCH=true
+  declare FLAG_PAGINATION=true
+  declare PAGE_SIZE=10
+
+  echo >&2
+
+  get_cursor_position() { declare POS; read -sdR -p $'\033[6n' POS; echo $POS | cut -c3-; }
+  min() { printf "%s\n" "${@:2}" | sort "$1" | head -n1; }
+  max() { printf "%s\n" "${@:2}" | sort "$1" | tail -n1; }
+  divide_round_up() { echo $((($1 + $2 - 1) / $2)); }
+  divide_round_down() { echo "$(awk "BEGIN {print int($1 / $2)}")"; }
+  # TODO: Seems to be and escaping issue where the key must assigned to a value
+  # DO: `declare key="$(read_keyboard)"; case "$key" in` ...)
+  # DON'T: `case "$(read_keyboard)" in` ...)
+  read_keyboard() {
+    declare k1
+    declare k2
+    declare k3
+    declare k4
+    declare k5
+
+    IFS=''
+    read -r -s -n1 k1
+    [[ $k1 == $'\033' ]] && read -r -s -n1 k2
+    [[ $k2 == [ ]] && read -r -s -n1 k3
+    [[ $k3 == [0-9] ]] && read -r -s -n1 k4
+    [[ $k4 == [0-9] ]] && read -r -s -n1 k5
+    unset IFS
+
+    declare key="${k1:-}${k2:-}${k3:-}${k4:-}${k5:-}"
+    case "$key" in
+      $'\033[A') echo UP ;;
+      $'\033[B') echo DOWN ;;
+      $'\033[D') echo LEFT ;;
+      $'\033[C') echo RIGHT ;;
+      $'\177') echo BACKSPACE ;;
+      $'\0') echo ENTER;;
+      *) echo "$key" ;;
+    esac
+  }
+
+  declare OPTIONS=("$@")
+  declare PAGE_SIZE=$([[ $FLAG_PAGINATION == true ]] && echo "10" || echo "${#OPTIONS[@]}")
+  declare LIST_LEN=$(min -g $PAGE_SIZE ${#OPTIONS[@]})
+  declare HEADER_LEN=$([[ $FLAG_SEARCH == true ]] && echo "2" || echo "0")
+  declare FOOTER_LEN=$([[ $FLAG_PAGINATION == true ]] && echo "2" || echo "0")
+  declare FILL_EMPTY=$({
+    declare max
+    for opt in "${OPTIONS[@]}"; do
+      [[ "${#opt}" -gt "$max" ]] && max="${#opt}"
+    done
+    printf '%*s' $max
+  })
+  declare CURSOR_POS=$(get_cursor_position)
+
+  declare selected=0
+  declare page=0
+  declare search=""
+  declare options=()
+  for opt in "${OPTIONS[@]}"; do
+    options+=("$(echo "$opt" | sed -e 's/  / /g' | tr '[[:upper:]]' '[[:lower:]]')")
+  done
+  declare options_filtered=()
+
+  get_paged_index() { declare i=$(($page * $PAGE_SIZE + $selected)); [[ "${i}" -lt 0 ]] && echo "0" || echo "$i"; }
+  get_selected_option() { echo "${options_filtered[$(get_paged_index)]:-}"; }
+  go_to() { printf "\033[$((${CURSOR_POS%;*}+${1:-0}));${2:-0}H" >&2; }
+  go_to_search() { go_to 0 "$((9+${#search}))"; }
+  print_option() { printf "${CLEAR_LINE}${C_RESET} %s\n" "$1" >&2; }
+  print_option_selected() { printf "${C_MAGENTA_BG}  %s%s  ${C_RESET}\n" "$1" "${FILL_EMPTY:${#1}}" >&2; }
+
+  draw() {
+    options_filtered=()
+    declare lower_search=$(echo $search | tr '[[:upper:]]' '[[:lower:]]')
+
+    for index in "${!OPTIONS[@]}"; do
+      if [[ "${options[$index]}" == *${lower_search}* ]]; then
+        options_filtered+=("${OPTIONS[$index]}")
+      fi
+    done
+
+    declare options_len="${#options_filtered[@]}"
+    declare visible_count=$(min -g "$options_len" $(($options_len - ($PAGE_SIZE * $page))))
+
+    if [[ $selected -ge $visible_count ]]; then
+      selected=$(($visible_count-1))
+    fi
+
+    go_to
+    if [[ $FLAG_SEARCH == true ]]; then
+      printf "${CLEAR_LINE}${C_RESET}%s: %s\n" "Search" "$search" >&2
+      printf "${CLEAR_LINE}${C_RESET}\n" >&2
+    fi
+
+    for (( i=0; i<$LIST_LEN; i++ )) {
+      declare page_i=$(($page * $PAGE_SIZE + $i))
+      declare option="${options_filtered[$page_i]:-}"
+      if [[ $page_i == $(get_paged_index) ]]; then
+        print_option_selected "$option"
+      else
+        print_option "$option"
+      fi
+    }
+
+    if [[ $FLAG_PAGINATION == true ]]; then
+      printf "${CLEAR_LINE}${C_RESET}\n" >&2
+      printf "${CLEAR_LINE}${C_RESET}%s %d-%d / %d\n" \
+        "Results:" \
+        $(( $PAGE_SIZE * $page + 1 )) \
+        $(min -g $(($PAGE_SIZE * ($page+1))) "$options_len") \
+        "$options_len" >&2
+    fi
+
+    [[ $FLAG_SEARCH == true ]] && go_to_search
+  }
+
+  draw
+  while true; do
+    # NOTE: `read_keyboard` out must be assigned. Inline use has escaping issue.
+    declare key=$(read_keyboard)
+    case "$key" in
+      ENTER)
+        declare opt="$(get_selected_option)"
+        [[ "$opt" != "" ]] && {
+          go_to "$(($HEADER_LEN + ${#options_filtered[@]} + $FOOTER_LEN))" 0
+          echo >&2
+          echo "$opt"
+          return
+        }
+        ;;
+      UP)
+        go_to "$(($HEADER_LEN + $selected))"
+        print_option "$(get_selected_option)"
+
+        ((selected--))
+        [[ "$selected" -lt 0 ]] && selected=$(max -g $(($(min -g "${#options_filtered[@]}" $LIST_LEN) - 1)) 0)
+
+        go_to "$(($HEADER_LEN + $selected))"
+        print_option_selected "$(get_selected_option)"
+        [ $FLAG_SEARCH == true ] && go_to_search
+        ;;
+      DOWN)
+        go_to "$(($HEADER_LEN + $selected))"
+        print_option "$(get_selected_option)"
+
+        ((selected++))
+        [[ "$selected" -ge $(min -g "${#options_filtered[@]}" $LIST_LEN) ]] && selected=0
+
+        go_to "$(($HEADER_LEN + $selected))"
+        print_option_selected "$(get_selected_option)"
+        [[ $FLAG_SEARCH == true ]] && go_to_search
+        ;;
+      LEFT)
+        if [[ $FLAG_PAGINATION == true ]]; then
+          ((page--))
+          [[ "$page" -lt 0 ]] && page=$(divide_round_down ${#options_filtered[@]} $PAGE_SIZE)
+          draw
+        fi
+        ;;
+      RIGHT)
+        if [[ $FLAG_PAGINATION == true ]]; then
+          ((page++))
+          [[ "$page" -gt "$(divide_round_down ${#options_filtered[@]} $PAGE_SIZE)" ]] && page=0
+          draw
+        fi
+        ;;
+      BACKSPACE)
+        if [[ $FLAG_SEARCH == true && "${#search}" -gt 0 ]]; then
+          search="${search:0:((${#search}-1))}"
+          draw
+        fi
+        ;;
+      *)
+        if [[ $FLAG_SEARCH == true ]]; then
+          page=0
+          search="${search}${key}"
+          draw
+        fi
+        ;;
+    esac
+  done
+}
+
 main() {
   # Setup
   trap _cleanup EXIT
@@ -379,6 +376,8 @@ main() {
   [[ $DEBUG == true ]] && log --debug "Debug enabled"
   [[ $VERBOSE == true ]] && log --info "Verbose enabled"
   echo
+
+  declare TICKET_NUMBER_REGEXP="[0-9]{$(($TICKET_NUMBER_LEN-1)),$TICKET_NUMBER_LEN}"
 
   declare git_branch=$(git branch --show-current)
   declare ticket_type
@@ -394,7 +393,7 @@ main() {
     if [[ "$yn_continue" == "y" ]]; then
       branch_name="${git_branch}"
       ticket_type=$(echo "${git_branch%/*}" | tr '[:lower:]' '[:upper:]')
-      ticket_number=$(echo "${git_branch}" | grep -E --only-matching --regexp '\d{5,6}')
+      ticket_number=$(echo "${git_branch}" | grep -E --only-matching --regexp "$TICKET_NUMBER_REGEXP")
     fi
     echo
   fi
@@ -406,9 +405,9 @@ main() {
   fi
 
   # Read ticket number
-  while [[ ! "${ticket_number:-}" =~ ^[0-9]{5}$ ]]; do
+  while [[ ! "${ticket_number:-}" =~ ^${TICKET_NUMBER_REGEXP}$ ]]; do
     printf "${C_MAGENTA}  %s #${C_RESET}" "${ticket_type}" >&2
-    read -r -e -n 5
+    read -r -e -n "$TICKET_NUMBER_LEN"
     ticket_number="${REPLY}"
   done
 
@@ -432,7 +431,7 @@ main() {
 
   if [[ -z $branch_name ]]; then
     branch_name=$(echo "${ticket_type}/${ticket_number}-${ticket_description}" \
-      | sed -E -e 's/:|;|,//g' -E -e 's/  | - / /g' -e 's/ /-/g' \
+      | sed -E -e 's/:|;|,//g' -E -e 's/ -| -|  / /g' -e 's/ */-/g' \
       | tr '[:upper:]' '[:lower:]' \
     )
   fi
