c8be9e3088412beca9784d85fc117c567d45d786
ndemasie <nathan.demasie@gmail.com>
2023-01-06 00:00:14 +0100

    task: expand key list and deprecate ansi file

--- templates/fn_read_keyboard.sh
+++ templates/fn_read_keyboard.sh
@@ -17,12 +17,39 @@ read_keyboard() {
 
   declare key="${k1:-}${k2:-}${k3:-}${k4:-}${k5:-}"
   case "$key" in
-    $'\033[A') echo UP ;;
-    $'\033[B') echo DOWN ;;
-    $'\033[D') echo LEFT ;;
-    $'\033[C') echo RIGHT ;;
-    $'\177') echo BACKSPACE ;;
+    $'\033[A') echo UP;;
+    $'\033[B') echo DOWN;;
+    $'\033[D') echo FORWARD;;
+    $'\033[C') echo BACK;;
+
+    $'\033[2~') echo INSERT;;
+    $'\033[3~') echo DELETE;;
+    $'\033[5~') echo PAGE_UP;;
+    $'\033[6~') echo PAGE_DOWN;;
+    $'\033[7~') echo HOME;;
+    $'\033[8~') echo END;;
+
+    $'\033[11~') echo F1;;
+    $'\033[12~') echo F2;;
+    $'\033[13~') echo F3;;
+    $'\033[14~') echo F4;;
+    $'\033[15~') echo F5;;
+    # $'\033[16~') echo Fx;;
+    $'\033[18~') echo F7;;
+    $'\033[17~') echo F6;;
+    $'\033[19~') echo F8;;
+    $'\033[20~') echo F9;;
+    $'\033[21~') echo F10;;
+    # $'\033[22~') echo Fy;;
+    $'\033[23~') echo F11;;
+    $'\033[24~') echo F12;;
+
+    $'\033') echo ESC;;
     $'\0') echo ENTER;;
+    $'\177') echo BACKSPACE;;
+    $'\40') echo SPACE;;
+    $'\033[I') echo TAB;;
+
     *) echo "$key" ;;
   esac
 }
