687dcce9b0d38a526c5f0c7664a6ce48706e334a
ndemasie <nathan.demasie@gmail.com>
2023-03-04 14:26:48 +0100

    paramatarize command

--- script-ffmpeg-to-mp4.sh
+++ script-ffmpeg-to-mp4.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-command -v "/usr/bin/ffmpeg" >/dev/null 2>&1 || { echo "ffmpeg is not installed"; exit 1; }
+ffmpeg_cmd=$(which ffmpeg)
+
+command -v $ffmpeg_cmd >/dev/null 2>&1 || { echo "ffmpeg is not installed"; exit 1; }
 
 path="${1:-}"
 output_path="${path%.*}.mp4"
 
-/usr/bin/ffmpeg -i "$path" -vcodec h264 -acodec mp2 "$output_path"
+$ffmpeg_cmd -i "$path" -vcodec h264 -acodec mp2 "$output_path"
