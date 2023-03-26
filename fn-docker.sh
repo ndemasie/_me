f404438f0ccc8b95b1edea9b0bf608d5c06abeba
ndemasie <nathan.demasie@gmail.com>
2023-03-26 22:17:52 +0200

    chore: format file for reading

--- fn-docker.sh
+++ fn-docker.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-function dclean() { docker ps --all --quiet | xargs docker rm && docker image ls --quiet --filter 'dangling=true' | xargs docker rmi; }
-function dfree() { docker volume ls --quiet --filter 'dangling=true' | xargs docker volume rm; }
-function dstop() { docker ps --all --quiet | xargs docker stop; }
+function dclean() {
+  docker ps --all --quiet \
+  | xargs docker rm && docker image ls --quiet --filter 'dangling=true' \
+  | xargs docker rmi;
+}
+
+function dfree() {
+  docker volume ls --quiet --filter 'dangling=true' \
+  | xargs docker volume rm;
+}
+
+function dstop() {
+  docker ps --all --quiet \
+  | xargs docker stop;
+}
