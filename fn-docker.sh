457a35886980731df9c8f4ae67a964c663d00393
ndemasie <nathan.demasie@gmail.com>
2023-03-26 16:01:44 +0200

    chore: shellcheck

--- fn-docker.sh
+++ fn-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-function dclean() { $(docker ps --all --quiet | xargs docker rm) && $(docker image ls --quiet --filter 'dangling=true' | xargs docker rmi); }
+function dclean() { docker ps --all --quiet | xargs docker rm && docker image ls --quiet --filter 'dangling=true' | xargs docker rmi; }
 function dfree() { docker volume ls --quiet --filter 'dangling=true' | xargs docker volume rm; }
 function dstop() { docker ps --all --quiet | xargs docker stop; }
