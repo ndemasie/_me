bc1067a082af760474cdf267d00c1348c4937ea7
ndemasie <nathan.demasie@gmail.com>
2023-11-12 21:43:06 +0100

    feat: new homebrew script

---
+++ setup-homebrew.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Install homebrew
+if ! command -v brew; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# # Language specifics
+# brew list deno &>/dev/null || brew install deno
+# brew list node &>/dev/null || brew install node
+# brew list ffmpeg &>/dev/null || brew install ffmpeg
+# brew list typescript &>/dev/null || brew install typescript
+# brew list terraform &>/dev/null || brew install terraform
+
+# # App
+# brew list rectangle &>/dev/null || brew install --cask rectangle
+# brew list maccy &>/dev/null || brew install --cask maccy
