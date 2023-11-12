a676dcaee80dda8cdff92b870a849cdfe56ebf90
ndemasie <nathan.demasie@gmail.com>
2023-11-12 22:09:17 +0100

    chore: add python3

--- setup-homebrew.sh
+++ setup-homebrew.sh
@@ -5,12 +5,16 @@ if ! command -v brew; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# # Setup
+# brew list ffmpeg &>/dev/null || brew install ffmpeg
+# brew list tmux &>/dev/null || brew install tmux
+# brew list python3 &>/dev/null || brew install python3
+
 # # Language specifics
 # brew list git &>/dev/null || brew install git
 # brew list jq &>/dev/null || brew install jq
 # brew list deno &>/dev/null || brew install deno
 # brew list node &>/dev/null || brew install node
-# brew list ffmpeg &>/dev/null || brew install ffmpeg
 # brew list typescript &>/dev/null || brew install typescript
 # brew list terraform &>/dev/null || brew install terraform
 
