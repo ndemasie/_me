d3632e5eff3da6288d41ec8ea2040a15ae96b74e
ndemasie <nathan.demasie@gmail.com>
2024-10-05 20:26:21 -0400

    chore: pref symlink folder

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -7,9 +7,7 @@ ln -s "${HOME}/_me/dotfiles/.zprofile" "${HOME}/.zprofile"
 # VSCode
 ln -sf "${HOME}/_me/app/VSCode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
 ln -sf "${HOME}/_me/app/VSCode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
-ln -sf "${HOME}/_me/app/VSCode/snippets/handlebars.json" "${HOME}/Library/Application Support/Code/User/snippets/handlebars.json"
-ln -sf "${HOME}/_me/app/VSCode/snippets/typescript.json" "${HOME}/Library/Application Support/Code/User/snippets/typescript.json"
-ln -sf "${HOME}/_me/app/VSCode/snippets/typescriptreact.json" "${HOME}/Library/Application Support/Code/User/snippets/typescriptreact.json"
+ln -sf "${HOME}/_me/app/VSCode/snippets" "${HOME}/Library/Application Support/Code/User/snippets"
 
 # Rectangle
 # TODO: Find default config path
