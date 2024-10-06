3fe8974c4d8334e91eb8723670b8c3ddfe229993
ndemasie <nathan.demasie@gmail.com>
2024-10-05 22:21:32 -0400

    feat: refactor fn_confirm a bit

--- templates/fn_confirm.sh
+++ templates/fn_confirm.sh
@@ -4,15 +4,23 @@ function confirm() {
   declare GO_LEFT="\033[80D"
   declare RESET="\033[0m"
   declare RED="\033[31m"
+  declare YELLOW="\033[33m"
 
   declare prompt="${1:-Continue?} [y/n]: "
 
   while true; do
-    read -k1 "?$prompt" REPLY
+    printf "${YELLOW}%s${RESET}" "$prompt"
+    read  -r -n 1
     case "${REPLY}" in
       y|Y) echo >&2; return 0 ;;
       n|N) echo >&2; return 1 ;;
       *) printf "      ${RED}%s${RESET}${GO_LEFT}" "Invalid input"
-    esac 
+    esac
   done
 }
+
+# DEMO
+# If script called directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  confirm "git rebase origin/main" && echo "Yes!" || echo "No!"
+fi
