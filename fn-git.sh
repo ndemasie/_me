17c274b6df8dbb12b016a3aead65675f806bf1b0
Nathan DeMasie <nathan.demasie@osapiens.com>
2024-11-02 02:48:20 +0100

    feature: add aliases and formatting

--- fn-git.sh
+++ fn-git.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+alias gp="confirm 'git push origin --force' && git push origin --force"
+alias gr="confirm 'git rebase origin/main' && git rebase origin/main"
+
 # delete local branches where remote is :gone
 function gprune() {
-  git fetch --prune --quiet;
-  git branch -vv | grep '^[^*].*: gone]' | awk '{print $1}' | xargs git branch -D;
+  git fetch --prune --quiet
+  git branch -vv | grep '^[^*].*: gone]' | awk '{print $1}' | xargs git branch -D
 }
