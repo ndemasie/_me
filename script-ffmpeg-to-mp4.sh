0781a78e5954ddbd6f79b02e9b48695e21e662d5
Nathan DeMasie <nathan.demasie@osapiens.com>
2024-11-02 03:01:56 +0100

    feat: add another branch to execute

--- script-ffmpeg-to-mp4.sh
+++ script-ffmpeg-to-mp4.sh
@@ -4,12 +4,17 @@ dir=$(dirname "${1:-}")
 filename=$(basename "${1:-}")
 output_file="${filename%.*}.mp4"
 
-if command -v ffmpeg &> /dev/null; then
+if command -v ffmpeg &>/dev/null; then
   ffmpeg -i "$dir/$filename" -vcodec h264 -acodec mp2 "$dir/$output_file"
   exit 0
 fi
 
-if command -v docker &> /dev/null; then
+if command -v /opt/homebrew/bin/ffmpeg &>/dev/null; then
+  /opt/homebrew/bin/ffmpeg -i "$dir/$filename" -vcodec h264 -acodec mp2 "$dir/$output_file"
+  exit 0
+fi
+
+if command -v docker &>/dev/null; then
   docker run --volume $dir:$dir --workdir $dir \
     jrottenberg/ffmpeg -i "$dir/$filename" -vcodec h264 -acodec mp2 "$dir/$output_file"
   exit 0
