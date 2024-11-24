4aa3fee43787c23a34c4231a8aa1d2025d063116
ndemasie <nathan.demasie@gmail.com>
2024-11-23 20:43:47 -0500

    comments

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,10 +4,10 @@ ln -s "${HOME}/_me/dotfiles/.bash_profile" "${HOME}/.bash_profile"
 ln -s "${HOME}/_me/dotfiles/.zprofile" "${HOME}/.zprofile"
 
 # App Config
-# VSCode
+# VSCode (https://code.visualstudio.com)
 ln -sf "${HOME}/_me/app/VSCode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
 ln -sf "${HOME}/_me/app/VSCode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
 ln -sf "${HOME}/_me/app/VSCode/snippets" "${HOME}/Library/Application Support/Code/User/snippets"
 
-# Rectangle
+# Rectangle Pro (https://rectangleapp.com/pro)
 ln -sf "${HOME}/_me/app/Rectangle/com.knollsoft.Rectangle.plist" "${HOME}/Library/Preferences/com.knollsoft.Rectangle.plist"
