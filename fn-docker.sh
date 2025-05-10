8fc7502b3836adb8ec6ea81c856aa923549b38fb
ndemasie <nathan.demasie@gmail.com>
2025-05-10 12:40:30 -0400

    refactor: fn-docker

--- fn-docker.sh
+++ fn-docker.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 function dclean() {
-  docker ps --all --quiet \
-  | xargs docker rm && docker image ls --quiet --filter 'dangling=true' \
-  | xargs docker rmi;
+  docker ps --all --quiet | xargs -r docker rm;
+  docker image ls --quiet --filter 'dangling=true' | xargs -r docker rmi;
 }
 
 function dfree() {
-  docker volume ls --quiet --filter 'dangling=true' \
-  | xargs docker volume rm;
+  docker volume ls --quiet --filter 'dangling=true' | xargs -r docker volume rm;
 }
 
 function dstop() {
-  docker ps --all --quiet \
-  | xargs docker stop;
+  docker ps --all --quiet | xargs -r docker stop;
 }
