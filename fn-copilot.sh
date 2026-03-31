cda0d69216b54b6e37ad997451447856305961cf
ndemasie <nathan.demasie@gmail.com>
2026-03-30 20:51:33 -0400

    rm fn-copilot

--- fn-copilot.sh
+++
@@ -1,7 +0,0 @@
-# if [ -n "$BASH_VERSION" ] && ! command -v ghcs &>/dev/null; then
-#   # eval "$(gh copilot alias -- bash)"
-# fi
-
-# if [ -n "$ZSH_VERSION" ] && ! command -v ghcs &>/dev/null; then
-#   # eval "$(gh copilot alias -- zsh)"
-# fi
