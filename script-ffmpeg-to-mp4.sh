0fc10072bd17f211f36924297e7df85a33b4b5dc
Nathan DeMasie <nathan.demasie@osapiens.com>
2026-04-03 18:32:27 +0200

    fix(script-ffmpeg-to-mp4): fix audio

--- script-ffmpeg-to-mp4.sh
+++ script-ffmpeg-to-mp4.sh
@@ -4,18 +4,20 @@ dir=$(dirname "${1:-}")
 filename=$(basename "${1:-}")
 output_file="${filename%.*}.mp4"
 
+cmd=(-i "$dir/$filename" -vcodec h264 -acodec aac "$dir/$output_file")
+
 if command -v ffmpeg &>/dev/null; then
-  ffmpeg -i "$dir/$filename" -vcodec h264 -acodec mp2 "$dir/$output_file"
+  ffmpeg "${cmd[@]}"
   exit 0
 fi
 
 if command -v /opt/homebrew/bin/ffmpeg &>/dev/null; then
-  /opt/homebrew/bin/ffmpeg -i "$dir/$filename" -vcodec h264 -acodec mp2 "$dir/$output_file"
+  /opt/homebrew/bin/ffmpeg "${cmd[@]}"
   exit 0
 fi
 
 if command -v docker &>/dev/null; then
   docker run --volume $dir:$dir --workdir $dir \
-    jrottenberg/ffmpeg -i "$dir/$filename" -vcodec h264 -acodec mp2 "$dir/$output_file"
+    jrottenberg/ffmpeg "${cmd[@]}"
   exit 0
 fi
