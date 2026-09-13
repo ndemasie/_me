f64aafc4aa67b79a0cd96565b459ff8b8dc10d50
ndemasie <nathan.demasie@gmail.com>
2026-09-12 21:24:09 -0400

    fix: sym link

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -6,12 +6,11 @@ ln -s "${HOME}/#me/dotfiles/.zprofile" "${HOME}/.zprofile"
 # ln -s "${HOME}/#me/dotfiles/.tmux.conf" "${HOME}/.tmux.conf"
 
 # Rectangle Pro (https://rectangleapp.com/pro)
-ln -sf "${HOME}/#me/app/Rectangle/com.knollsoft.Rectangle.plist" "${HOME}/Library/Preferences/com.knollsoft.Rectangle.plist"
+# ln -sf "${HOME}/#me/app/Rectangle/com.knollsoft.Rectangle.plist" "${HOME}/Library/Preferences/com.knollsoft.Rectangle.plist"
 
 # VSCode (https://code.visualstudio.com)
 ln -sf "${HOME}/#me/app/VSCode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
 ln -sf "${HOME}/#me/app/VSCode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
-ln -sf "${HOME}/#me/app/VSCode/snippets" "${HOME}/Library/Application Support/Code/User/snippets"
 
 # Workflows
 ln -s "${HOME}/#me/automator/compress-pdf.workflow" "${HOME}/Library/Services/compress-pdf.workflow"
